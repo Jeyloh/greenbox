aecedea725590fa8d735eb9a7313907ea0f8d7c1
Jeyloh <jlh.94@hotmail.com>
2016-12-05 17:08:56 +0000

    changing branches

---
+++ greenbox-master/login.php
@@ -0,0 +1,43 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<title>The Greenhouse - Login</title>
+	<meta charset="utf-8">
+	<link rel="stylesheet" href="css/login.css">
+	<script src="js/jquery-3.1.1.min.js"></script>
+<?php
+	include('functions.php');
+	isLoggedIn();
+	if(isLoggedIn()) {
+		if ($_SESSION['admin'] == true) {
+			header("location:adminpage.php");
+		}
+		else {
+			header("location:userpage.php");
+		}
+	}
+
+?>
+</head>
+<body>
+
+	<div id="outer-image">
+		<!-- Add tabs here for either Creating user or logging in -->
+		<div id="wrapper">
+			<main>
+				<div id="loginform">
+				<!-- INSERT LOGIN FORM HERE -->
+					<h2>LOG IN</h2>
+					<form method="POST" action="processLogin.php">
+						<label>Username </label><br>
+						<input type=text name=loginusername size=30><br>
+						<label>Password</label><br>
+						<input type=password name=loginpassword size=30><br>
+						<input type=submit id="loginsubmit" value=Login><br>
+					</form>
+				</div>
+			</main>
+		</div>
+	</div>
+</body>
+</html>
---
+++ greenbox-master/userpage.php
@@ -0,0 +1,14 @@
+<?php $title = "Userpage"; include("top.php");?>
+<div class="container-fluid">
+	<div class="col-md-8">
+		<h3>User Page</h3>
+		<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit...</p>
+		<p>Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris...</p>
+	</div>
+	<div class="col-md-4">
+		<img src="../resources/art.jpg" class="img-responsive img-rounded">
+
+	</div>
+</div>
+
+<?php include("bottom.php");?>
