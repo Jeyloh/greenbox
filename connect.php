476011a664f078c3f05f937d0c1435b388d57212
Jeyloh <jlh.94@hotmail.com>
2016-12-10 19:07:19 +0000

    added heroku support

--- connect.php
+++ connect.php
@@ -18,7 +18,7 @@ if ($con->connect_error) {
     $cleardb_password = $cleardb_url["pass"];
 	$cleardb_db = substr($cleardb_url["path"], 1);
     $con = new mysqli($cleardb_server, $cleardb_username, $cleardb_password, $cleardb_db);
-
+    $using_heroku = true;
     if ($con->connect_error) {
     	die("Connection to heroku server failed: " . $con->connect_error);
     }
@@ -26,15 +26,28 @@ if ($con->connect_error) {
 }
 
 // Create database
-$createDB = "CREATE DATABASE IF NOT EXISTS $db";
+if($using_heroku){
+    $createDB = "CREATE DATABASE IF NOT EXISTS $cleardb_db";
+} else {
+    $createDB = "CREATE DATABASE IF NOT EXISTS $db";
+}
+
 if ($con->query($createDB) === TRUE) {
-	if($_SERVER['REQUEST_URI'] == "greenbox/database-setup.php") {
-		echo "<br>Database created successfully";
-	}
+    if($_SERVER['REQUEST_URI'] == "greenbox/database-setup.php") {
+        echo "<br>Database created successfully";
+    }
 } else {
     echo "<br>Error creating database: " . $con->error;
 }
 
 $con = new mysqli($servername, $username, $password, $db);
+
+if($using_heroku) {
+    $con = new mysqli($servername, $username, $password, $cleardb_db);
+}
+else {
+    $con = new mysqli($servername, $username, $password, $db);
+}
+
 
 ?>
